431281cc4dbe28f304e4925611dda6e62a34ed02
ia-traders <irfanali555567@gmail.com>
2026-08-03 18:45:36 +0500

    Update admin.js

--- admin.js
+++ admin.js
@@ -7,59 +7,92 @@ import {
 } from "./firebase.js";
 
 
-
 const requestsDiv = document.getElementById("requests");
 
 
 async function loadRequests(){
 
   const snapshot = await getDocs(
-    collection(db,"investmentRequests")
+    collection(db, "investmentRequests")
   );
 
 
   requestsDiv.innerHTML = "";
 
 
-  snapshot.forEach((doc)=>{
+  snapshot.forEach((item)=>{
 
-    const data = doc.data();
+    const data = item.data();
 
 
     requestsDiv.innerHTML += `
 
     <div class="request">
 
-    <p><b>Email:</b> ${data.email}</p>
+      <p><b>Email:</b> ${data.email}</p>
 
-    <p><b>Plan:</b> Rs.${data.plan}</p>
+      <p><b>Plan:</b> Rs.${data.plan}</p>
 
-    <p><b>Amount:</b> Rs.${data.amount}</p>
+      <p><b>Amount:</b> Rs.${data.amount}</p>
 
-    <p><b>Sender Name:</b> ${data.senderName}</p>
+      <p><b>Sender Name:</b> ${data.senderName}</p>
 
-    <p><b>Sender Number:</b> ${data.senderNumber}</p>
+      <p><b>Sender Number:</b> ${data.senderNumber}</p>
 
-    <p><b>Status:</b> ${data.status}</p>
+      <p><b>Status:</b> ${data.status}</p>
 
-    <button>
-    Approve
-    </button>
 
-    <button class="reject">
-    Reject
-    </button>
+      <button onclick="approveRequest('${item.id}')">
+      Approve
+      </button>
+
+
+      <button class="reject" onclick="rejectRequest('${item.id}')">
+      Reject
+      </button>
 
 
     </div>
 
     `;
 
-
   });
 
+}
+
+
+window.approveRequest = async function(id){
+
+  await updateDoc(
+    doc(db,"investmentRequests",id),
+    {
+      status:"Approved"
+    }
+  );
+
+  alert("Request Approved");
+
+  loadRequests();
 
 }
+
+
+
+window.rejectRequest = async function(id){
+
+  await updateDoc(
+    doc(db,"investmentRequests",id),
+    {
+      status:"Rejected"
+    }
+  );
+
+  alert("Request Rejected");
+
+  loadRequests();
+
+}
+
 
 
 loadRequests();
