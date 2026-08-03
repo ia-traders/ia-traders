52e5c0de22a97e369fddaf101a156b62d60b542f
ia-traders <irfanali555567@gmail.com>
2026-08-03 12:41:04 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -10,6 +10,7 @@ const firebaseConfig = {
 };
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
-const app = initializeApp(firebaseConfig);
+export { app, db };
